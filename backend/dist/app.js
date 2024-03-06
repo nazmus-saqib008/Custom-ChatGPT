c6f2742fab49d3c56dde657ffcf8b008ad4e1f39
nazmus-saqib008 <iamnazmussaqib@outlook.com>
2024-03-07 01:47:00 +0530

    deployment settings

--- backend/dist/app.js
+++ backend/dist/app.js
@@ -4,14 +4,22 @@ import morgan from "morgan";
 import appRouter from "./routes/index.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from "path";
 // config();
 dotenv.config();
+const __dirname= path.resolve();
 const app = express();
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(express.static(path.join(__dirname,'frontend','dist')));
+
+app.get('*', (req, res)=>{
+    res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'))
+})
+// app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
 //remove it in production
 app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(morgan("dev"));
+
 app.use("/api/v1", appRouter);
 export default app;
 //# sourceMappingURL=app.js.map
